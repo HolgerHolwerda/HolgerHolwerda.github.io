50b169ea9536aa320ac6390a8f2f7ffeb7c7b6b0
Holger <holgerholwerda@gmail.com>
2020-10-05 09:58:22 +0200

    bounceball lavet

--- Test/sketch.js
+++ Test/sketch.js
@@ -1,19 +1,53 @@
-let CX = 200;
-let retning = 10
+let CX = 0;
+let radius = 50;
+let hastighed = 5;
 
 function setup() {
-  createCanvas(400, 450);
+  createCanvas(500, 400);
 }
 
 function draw() {
   background(220);
-  
-let radius = 100
 
-circle(CX,200,radius)
+// op og ned:
+// fill(150,100,400)  
+// circle(200,CX,radius)
 
-if (CX < width+radius) { CX = CX + 3} 
-else if (CX > width) {CX=CX-400-radius}
+// if (CX < length + radius/2) {hastighed = 5}
+// if (CX > 400 - radius/2) {hastighed = -5}
+
+// CX = CX + hastighed
+
+//__________________________________________________
+
+// frem og tilbage:
+// fill(0)  
+// circle(CX,200,radius)
+
+// if (CX > width - radius/2) {hastighed = -3}
+// if (CX < 0 + radius/2) {hastighed = 3}
+
+// CX = CX + hastighed
+
+
+//_________________________________________________
+
+
+fill(150,100,400)  
+circle(200,CX,radius)
+
+if (CX < length + radius/2) {hastighed = 5}
+if (CX > mouseY - radius/2) {hastighed = -5}
+
+CX = CX + hastighed
+
+
+
+let rectL = 100;
+let rectB = 15;
+
+fill(0)
+rect(mouseX - rectL/2,mouseY - rectB/2,rectL,rectB)
 
 
 }
