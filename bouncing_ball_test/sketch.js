92c92760a15f7396f0d35bd9cb0b04c1ed9b7d66
Holger <holgerholwerda@gmail.com>
2020-10-08 14:57:06 +0200

    forsøg på bedre bounce test

--- bouncing_ball_test/sketch.js
+++ bouncing_ball_test/sketch.js
@@ -14,18 +14,13 @@ function draw() {
   fill(0)  
   circle(CX,200,radius)
   
-  if (CX > width - radius/2) {position = -hastighed}
-  if (CX < 0 + radius/2) {position = hastighed}
+  if (CX > width - radius/2) {position = -hastighed};
+  if (CX < 0 + radius/2) {position = hastighed};
   
-  if (position > 0) {if (keyIsPressed) {position = hastighed + hastighed_ændring} }
-  if (position < 0) {if (keyIsPressed) {position = hastighed - hastighed_ændring} }
+  if (position >= 0) {if (mouseIsPressed) {position = hastighed + hastighed_ændring} };
+  if (position <= 0) {if (mouseIsPressed) {position = hastighed - hastighed_ændring} };
 
   print(position)
 
   CX = CX + position
-}
-
-function keyIsPressed(){
-
-  
 }
