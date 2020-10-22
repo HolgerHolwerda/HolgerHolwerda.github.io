cef542434281c552f5407e618b7578a14fa1df6e
Holger <holgerholwerda@gmail.com>
2020-10-22 12:55:13 +0200

    bounce forbedring

--- bouncing_ball_test/sketch.js
+++ bouncing_ball_test/sketch.js
@@ -4,7 +4,7 @@ let hastighed = 4;
 let hastighed_ændring = 3;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
@@ -15,11 +15,10 @@ function draw() {
   circle(CX,200,radius)
   
   if (CX > width - radius/2) {position = -hastighed};
-  if (CX < 0 + radius/2) {position = hastighed};
+  if (CX < radius/2) {position = hastighed};
   
-  if (position >= 0) {if (mouseIsPressed) {position = hastighed + hastighed_ændring} };
- // if (position <= 0) 
- else {if (mouseIsPressed) {position = hastighed - hastighed_ændring} };
+  if (position >= 0) {if (mouseIsPressed) {position = hastighed + hastighed_ændring};}
+  if (position <= 0)  {if (mouseIsPressed) {position = hastighed - hastighed_ændring} ;}
 
   print(position)
 
