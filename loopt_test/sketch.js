a0ca5f4b07fcb4a3052dafda54f623ff70fd7526
Holger <holgerholwerda@gmail.com>
2020-10-22 14:56:10 +0200

    loop opgaver

--- loopt_test/sketch.js
+++ loopt_test/sketch.js
@@ -3,9 +3,27 @@
 function setup() {
   createCanvas(400, 400);
 
-let x = 42
-console.log(x)
+//opgave 1
+  // let x = 10
+
+// while (x > 0) {
+// console.log(x)
+// x--;
+// }
 
 
-}
+//opgave 2
+// let n = 20
+// let x = 1
+// let y = 1
+
+// do {
+//   x++
+//   y = y+x
+// } while (x<n)
+// console.log(y)
+// }
+
+
+//opgave 3
 
