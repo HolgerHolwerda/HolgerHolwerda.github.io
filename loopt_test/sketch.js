cae4e65f60615147681d164c22b3cc59bc64a290
Holger <holgerholwerda@gmail.com>
2020-10-24 11:08:18 +0200

    loop opgaver

--- loopt_test/sketch.js
+++ loopt_test/sketch.js
@@ -28,15 +28,35 @@ function setup() {
 //opgave 3
 
 
+// let x = 2
+// let y = 1
+// let z = 0
+
+// do { 
+
+//   z = x*y
+//   y++
+//   console.log(z)
+
+// }while (y<=10)
+
 let x = 2
 let y = 1
 let z = 0
 
 do { 
+
+  if (y<=10) {
   z = x*y
   y++
-  console.log(z)
+  console.log(z)}
+  
 
-}while (y<=10)
+
+}while (x<=10)
+
+
+
 
 }
+
