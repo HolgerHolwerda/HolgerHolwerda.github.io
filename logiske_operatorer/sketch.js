e73e3f63d38f3db54820348a4aba18de5f70c226
Holger <holgerholwerda@gmail.com>
2020-10-29 15:04:39 +0100

    logiske operatorer

---
+++ logiske_operatorer/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(220);
+
+// if(mouseX > width/2 && mouseY > height/2) {fill ('white')}
+//   else if (mouseX < width/2 && mouseY < height/2) {fill('white')}
+//   else {fill('red')}
+
+if(mouseX > width/2 && mouseY > height/2 || mouseX < width/2 && mouseY < height/2) {
+  fill ('white');
+}
+  else {fill('red')}
+  
+  line(width/2,0,width/2,height)
+  line(0,height/2,width,height/2)
+  
+  circle(mouseX,mouseY,30);
+
+}
