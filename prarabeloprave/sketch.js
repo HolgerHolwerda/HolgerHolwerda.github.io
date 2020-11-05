1052d92b44eaff014de99be388ebd44d25529c22
Holger <holgerholwerda@gmail.com>
2020-11-05 14:03:56 +0100

    parabel

--- prarabeloprave/sketch.js
+++ prarabeloprave/sketch.js
@@ -6,23 +6,29 @@ function setup() {
 function draw() {
   background(220);
 
-  let x1 = 100
-  let x2 = 210
-  let x3 = 150
-  let y1 = 100
-  let y2 = 100
+  let x1 = width/2-150
+  let x2 = width/2+150
+  let x3 = width/2
+  let y1 = 60
+  let y2 = 60
   let y3 = mouseY
+
+  let n = mouseX
 
 
 point(x1,y1);
 point(x2,y2);
 point(x3,y3);
 
-point(sqrt(20),100);
 
 line(x1,y1,x3,y3);
 line(x2,y2,x3,y3);
 
-map()
+let a = 1
+let b = 1
+let x = 1
+let y = a*x+b
+
+map(y,10,300,10,300,true)
 
 }
