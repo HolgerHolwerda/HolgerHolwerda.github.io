0511352ccda664a7399a5ce497830de887fd9154
Holger <holgerholwerda@gmail.com>
2020-11-05 13:51:40 +0100

    parabel

--- prarabeloprave/sketch.js
+++ prarabeloprave/sketch.js
@@ -1,9 +1,6 @@
 function setup() {
   createCanvas(500, 400);
 
-
-
-
 }
 
 function draw() {
@@ -14,15 +11,18 @@ function draw() {
   let x3 = 150
   let y1 = 100
   let y2 = 100
-  let y3 = 300
+  let y3 = mouseY
+
 
 point(x1,y1);
 point(x2,y2);
-point(x3,y3)
-strokeWeight(3);
+point(x3,y3);
 
-line(x1,y1,x3,y3)
-line(x2,y2,x3,y3)
+point(sqrt(20),100);
 
+line(x1,y1,x3,y3);
+line(x2,y2,x3,y3);
+
+map()
 
 }
