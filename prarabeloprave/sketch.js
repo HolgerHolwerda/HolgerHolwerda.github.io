0fef64dbd961d104dffff146a84de89cd16b2ff3
Holger <holgerholwerda@gmail.com>
2020-11-05 13:09:17 +0100

    parabel

--- prarabeloprave/sketch.js
+++ prarabeloprave/sketch.js
@@ -1,9 +1,28 @@
 function setup() {
   createCanvas(500, 400);
+
+
+
+
 }
 
 function draw() {
   background(220);
+
+  let x1 = 100
+  let x2 = 210
+  let x3 = 150
+  let y1 = 100
+  let y2 = 100
+  let y3 = 300
+
+point(x1,y1);
+point(x2,y2);
+point(x3,y3)
+strokeWeight(3);
+
+line(x1,y1,x3,y3)
+line(x2,y2,x3,y3)
 
 
 }
