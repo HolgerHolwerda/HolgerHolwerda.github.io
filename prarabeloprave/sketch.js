c928d3a8b3c2a5eafe5b0211acf6fbdaf691f798
Holger <holgerholwerda@gmail.com>
2020-11-05 15:05:12 +0100

    parabelfungerer

--- prarabeloprave/sketch.js
+++ prarabeloprave/sketch.js
@@ -22,22 +22,24 @@ function draw() {
   line(px1,py1,px3,py3);
   line(px2,py2,px3,py3);
   
-  let n = map(mouseX,0,width,2,10,true)
+  let n = map(mouseX,0,width,2,15,true)
   n = round(n)
   
 
 for(let i = 0; i <= n; i++ ){
   const x1 = map(i,0,n,px1,px3,true)
   const y1 = map(i,0,n,py1,py3,true)
-  const x2 = map(i,0,n,px2,px3,true)
-  const y2 = map(i,0,n,py2,py3,true)
-
+  
+  const x2 = map(i,0,n,px3,px2,true)
+  const y2 = map(i,0,n,py3,py2,true)
+  
+  
   circle(x1,y1,10)
   circle (x2,y2,10)
-
   
-  //line(x1,y1,px2,py2)
-  //line(x2,y2,px1,py1)
+  line(x1,y1,x2,y2)
+
+
   
 }
 
