27f99a264aa78564cfcb6ef560317d621cd08abd
Holger <holgerholwerda@gmail.com>
2020-11-19 13:41:33 +0100

    bounce all nu med også med y værdier

--- bounce_all/sketch.js
+++ bounce_all/sketch.js
@@ -2,9 +2,9 @@ let diameter = 50;
 let radius = diameter/2;
 
 let xSpeed = [-1,5,3];
-//let ySpeed = [2,-4,1];
+let ySpeed = [2,-4,1];
 let xValues = [50,150,200];
-//let yValues = [100,200,300];
+let yValues = [100,200,300];
 
 function setup() {
   createCanvas(600, 500);
@@ -14,26 +14,22 @@ function setup() {
 function draw() {
   background(200);
   
-  // for(let i = 0 ; i < xValues.length && i < yValues.length; i++){
-    for(let i = 0 ; i < xValues.length; i++){
+  for(let i = 0 ; i < xValues.length && i < yValues.length; i++){
       if(xValues[i] > width - radius || xValues[i] < 0 + radius){
         xSpeed[i] = -xSpeed[i]
       }
       
-      // if(yValues[i] > length - radius || yValues[i] < 0 + radius){
-        //   ySpeed = -ySpeed
-        // }
+       if(yValues[i] > height - radius || yValues[i] < 0 + radius){
+         ySpeed[i] = -ySpeed[i]
+         }
     
         xValues[i] += xSpeed[i];
-        // yValues[i] += ySpeed;
+        yValues[i] += ySpeed[i];
         
-    
-        
-        circle(xValues[i],100,diameter);
+        circle(xValues[i],yValues[i],diameter);
       }
       
   
-  //circle(xValues[i],yValues[i],diameter);
   
   
 }
