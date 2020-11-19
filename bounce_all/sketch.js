159d55a659c283d4b10bffff8f22b923af2281d1
Holger <holgerholwerda@gmail.com>
2020-11-19 13:38:00 +0100

    bounce all første udkast

--- bounce_all/sketch.js
+++ bounce_all/sketch.js
@@ -1,7 +1,10 @@
-let radius = 50;
-let hastighed = 15;
+let diameter = 50;
+let radius = diameter/2;
+
+let xSpeed = [-1,5,3];
+//let ySpeed = [2,-4,1];
 let xValues = [50,150,200];
-let yValues = [100,200,300];
+//let yValues = [100,200,300];
 
 function setup() {
   createCanvas(600, 500);
@@ -11,18 +14,26 @@ function setup() {
 function draw() {
   background(200);
   
+  // for(let i = 0 ; i < xValues.length && i < yValues.length; i++){
+    for(let i = 0 ; i < xValues.length; i++){
+      if(xValues[i] > width - radius || xValues[i] < 0 + radius){
+        xSpeed[i] = -xSpeed[i]
+      }
+      
+      // if(yValues[i] > length - radius || yValues[i] < 0 + radius){
+        //   ySpeed = -ySpeed
+        // }
+    
+        xValues[i] += xSpeed[i];
+        // yValues[i] += ySpeed;
+        
+    
+        
+        circle(xValues[i],100,diameter);
+      }
+      
   
-  //circle(xValues,200,radius)
+  //circle(xValues[i],yValues[i],diameter);
   
-  for(let i = 0 ; i < xValues.length ; i++){
-    
-    xValues[i] += hastighed;
-    //yValues[i] += hastighed;
-    
-    
-    circle(xValues[i],yValues[i],radius)
-    if(xValues[i] < radius/2 || xValues[i] > width - radius/2) {hastighed=-hastighed};
-    
-  }
-
+  
 }
