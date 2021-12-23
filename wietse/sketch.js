57a48da525dee6160449c19f4bf175bc9704091f
Holger <holgerholwerda@gmail.com>
2021-12-23 11:24:51 +0100

    wietses skattejagt tilføjet

---
+++ wietse/sketch.js
@@ -0,0 +1,31 @@
+// function setup() {
+//   createCanvas(400, 400);
+
+// }
+
+// function draw() {
+//   background(220);
+
+
+//   // let v = 90
+//   // let r = (v/360)*2*Math.PI
+//   // // let r = 180*v/Math.PI
+//   // let sinv = Math.sin(r)
+//   // let cosv = Math.cos(r)
+//   // let tanv = Math.tan(r)
+   
+//   // console.log('sinv=', sinv, 'cosv=', cosv, 'tanv=', tanv)
+
+//   // console.log(v)
+//   // console.log(r)
+
+  
+//   // line(200,200,300,200)
+  
+//   circle(200,200,200)
+  
+//   // let s = createvector(100,100)
+//   let c = 200
+//   line(200,300,c,c)
+  
+// }
